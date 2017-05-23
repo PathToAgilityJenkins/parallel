75a10946dedb158ec6cd5f202dcdcdea14fef807
Fox <kfox@ICC10308.local>
2017-05-23 15:50:14 -0400

    Initialize parallel Maven projects

---
+++ alpha/src/main/java/klf/test/jenkins/alpha/App.java
@@ -0,0 +1,13 @@
+package klf.test.jenkins.alpha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ beta/src/main/java/klf/test/jenkins/beta/App.java
@@ -0,0 +1,13 @@
+package klf.test.jenkins.beta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
